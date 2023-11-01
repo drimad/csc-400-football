76f4f6be5ed84b8945110a210f033a17b697daf1
Imad Moukadem <imad.moukadem@mu.edu.lb>
2023-11-01 07:12:46 -0700

    starting point

---
+++ Web/Controllers/TeamsController.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.CompilerServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers;
+
+// /teams/
+public class TeamsController : Controller
+{
+    public List<string> Teams { get; set; } = new(); // maintain encapsulation
+
+    public TeamsController()
+    {
+        Teams.Add("Barcelona");
+        Teams.Add("PSG");
+        Teams.Add("BayerMunich");
+    }
+
+    public IActionResult Index()
+    {
+        ViewBag.Teams = Teams;
+        return View();
+    }
+}
