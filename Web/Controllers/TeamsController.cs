4e089ac5ed9d15086bc98e213e4cea65e701c49f
Imad Moukadem <imad.moukadem@mu.edu.lb>
2023-11-13 06:50:27 -0800

    Completed Add & Edit functionalities; Fixed Icons

--- Web/Controllers/TeamsController.cs
+++ Web/Controllers/TeamsController.cs
@@ -6,25 +6,56 @@ namespace Web.Controllers;
 // /teams/
 public class TeamsController : Controller
 {
-    public static List<string> Teams { get; set; } = new(); // maintain encapsulation
-
-    public TeamsController()
+    private static List<string> Teams { get; set; } = new()
     {
-        Teams.Add("Barcelona");
-        Teams.Add("PSG");
-        Teams.Add("BayerMunich");
-    }
+        "Barcelona", "PSG", "BayerMunich"
+    }; // maintain encapsulation
 
+
+    // /teams/index
+    [HttpGet]
     public IActionResult Index()
     {
         ViewBag.Teams = Teams;
         return View();
     }
 
+    // /teams/delete/id
+    [HttpGet]
     public IActionResult Delete(int id)
     {
         // Delete the team from the list based on its index
         Teams.RemoveAt(id);
+        return RedirectToAction("Index");
+    }
+
+    // /teams/add
+    [HttpGet]
+    public IActionResult Add()
+    {
+        return View();
+    }
+
+    // /teams/add
+    [HttpPost]
+    public IActionResult Add(string teamName)
+    {
+        Teams.Add(teamName);
+        return RedirectToAction("Index");
+    }
+
+    [HttpGet]
+    public IActionResult Edit(int id)
+    {
+        // retrieve the current team name at the specified id index
+        // send both id & current team name to the view through ViewBag
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Edit(int id, string newTeamName)
+    {
+        // replace the current team name with the new team name at the specified id/index
         return RedirectToAction("Index");
     }
 }
