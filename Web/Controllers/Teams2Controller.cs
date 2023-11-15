25e6f0a2e802ca0288b5d4bdfa8bba6a483fd5d4
Imad Moukadem <imad.moukadem@mu.edu.lb>
2023-11-15 06:49:50 -0800

    Added Team class & Teams2Controller.cs

---
+++ Web/Controllers/Teams2Controller.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Web.Models;
+
+namespace Web.Controllers;
+
+public class Teams2Controller : Controller
+{
+    private static List<Team> Teams { get; set; } = new()
+    {
+        new Team()
+        {
+            Id = 0,
+            Name = "Barcelona",
+            Location = "Spain",
+            NumberOfPlayers = 36,
+            Income = 1000000000000
+        },
+        new Team()
+        {
+            Id = 1,
+            Name = "PSG",
+            Location = "France",
+            NumberOfPlayers = 34,
+            Income = 10000000000000
+        },
+        new Team()
+        {
+            Id = 2,
+            Name = "Liverpool",
+            Location = "UK",
+            NumberOfPlayers = 32,
+            Income = 10000000000
+        }
+    };
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
---
+++ Web/Models/Team.cs
@@ -0,0 +1,10 @@
+﻿namespace Web.Models;
+
+public class Team
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = "";
+    public string Location { get; set; } = string.Empty;
+    public int NumberOfPlayers { get; set; }
+    public decimal Income { get; set; }
+}
