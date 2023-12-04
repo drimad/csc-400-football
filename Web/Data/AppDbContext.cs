a62e5f6b2c5cb373bdd196f91f1b2f0b8fbc55c6
Imad Moukadem <imad.moukadem@mu.edu.lb>
2023-12-04 07:57:21 -0800

    Added EntityFramework

---
+++ Web/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Transactions;
+using Microsoft.EntityFrameworkCore;
+using Web.Models;
+
+namespace Web.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Team> Teams { get; set; }
+}
--- Web/Program.cs
+++ Web/Program.cs
@@ -1,7 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using Web.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// Add Entity Framework to my app
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("sqlite")));
 
 var app = builder.Build();
 
